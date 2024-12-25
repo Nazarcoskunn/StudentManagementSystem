e16f1098bbf3ba9252a0a81dd85d7f9e65282b28
Nazarcoskunn <coskunnazar73@gmail.com>
2024-12-25 23:13:46 +0300

    Proje dosyası ekle.

---
+++ Models/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace StudentManagementSystem.Models
+{
+    public class Student
+    {
+        public int Id { get; set;}
+        public string? Name { get; set; }
+        public int StudentId { get; set; }
+        public string? Class { get; set; }
+    }
+}
